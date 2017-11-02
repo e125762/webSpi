46afb8525149131ab4d9b8d652a91ccaa564d58d
rails5_vagrant <e125762@ie.u-ryukyu.ac.jp>
2017-11-02 11:26:38 +0100

    make easy question

---
+++ app/helpers/sonneki_helper.rb
@@ -0,0 +1,2 @@
+module SonnekiHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'sonneki/q1_k'
+
+  get 'sonneki/q2_k'
+
   root 'spi_pages#index'
   get 'spi_pages/sonneki', as: :sonneki
 end
