fe18b636fcde86412664cb9cc2ac934bf59107f1
rails5_vagrant <e125762@ie.u-ryukyu.ac.jp>
2017-11-29 07:45:55 +0100

    select using vue.js

--- app/javascript/packs/explain_vue.js
+++ app/javascript/packs/explain_vue.js
@@ -3,6 +3,21 @@ import Vue from 'vue/dist/vue.esm.js'
 var vm = new Vue({
   el: '#explain',
   data: {
-    show: true
+    show: false,
+    flag: null
+  },
+  methods: {
+      updateAns: function() {
+      //選択初回のみ動作
+      if (!this.show) {
+          var answer = document.getElementById("answer").innerHTML
+          if (event.target.innerHTML === answer ) {
+          this.flag = 1;
+          } else {
+          this.flag = 0;
+          }
+          this.show = true
+      }
+      }
   }
 })
